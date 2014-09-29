d8c5d15ce68f708ed0b3963e98e23e026316ae77
josephktcheung <josephktcheung@gmail.com>
2014-09-29 11:48:48 +0800

    Set up storyboard and create relevant controllers

---
+++ remember-objc/Controllers/DevicesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DevicesTableViewController.h
+//  remember-objc
+//
+//  Created by Joseph Cheung on 29/9/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DevicesTableViewController : UITableViewController
+
+@end
