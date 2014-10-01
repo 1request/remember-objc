ab3c27cddfe003957dcbf4f0042837293200f259
Harry Ng <harryworld@gmail.com>
2014-10-02 00:29:51 +0800

    Add Crashlytics

--- remember-objc/AppDelegate.m
+++ remember-objc/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Crashlytics/Crashlytics.h>
 
 @interface AppDelegate ()
 
@@ -16,6 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [Crashlytics startWithAPIKey:@"a73df0ceadf9f0995f97da85f3a3ca791c3e0de1"];
     
     return YES;
 }
