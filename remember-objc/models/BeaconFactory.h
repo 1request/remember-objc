fdedeeeb2b697c7ba3c1933607a266cd52e523e8
josephktcheung <josephktcheung@gmail.com>
2014-10-02 11:00:11 +0800

    Add Beacon Factory to return beacon regions to be ranged

---
+++ remember-objc/models/BeaconFactory.h
@@ -0,0 +1,15 @@
+//
+//  BeaconFactory.h
+//  remember-objc
+//
+//  Created by Joseph Cheung on 2/10/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BeaconFactory : NSObject
+
++ (NSArray *)beaconsRegionsToBeRanged;
+
+@end
